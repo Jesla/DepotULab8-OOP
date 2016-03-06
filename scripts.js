9a6b6c79fddd3ecb9dcd9fe64f97031ae5f0836b
Jesla <jesslar415@gmail.com>
2016-03-05 18:21:37 -0600

    Buttons and Dice Created

---
+++ scripts.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    addEventListener("click", buttonClick1);
+
+function buttonClick1() {
+    var divBox = document.createElement("div");
+    document.getElementById('mainDiv');
+    divBox.className = "dieDesign";
+    document.body.appendChild(divBox);
+    
+    function buttonClick2() {    
+    var Die = new Object();
+    Die.number=(Math.floor((Math.random() * 6) + 1));
+    //tie the new number to the div using innerHTML maybe?
+    
+}  
+}
+});
+
+var diceNumber = []; //this has to stay outside and be global
+
+// function(e) {
+// e.target.innerHTML = (Math.floor((Math.random() * 6) + 1));
+
+// to loop - use for statement
+    
+// text.randomNumber
