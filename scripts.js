de8c74dd3a7273b752fd12025298c66d81bec08c
Jesla <jesslar415@gmail.com>
2016-03-07 09:58:04 -0600

    After Class Demo

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
-var addDie = function() {
-        var dieDesign = document.getElementById("mainDiv");
-        var divBox = document.createElement("div");
-        divBox.className = "dieDesign";
-        dieDesign.appendChild(divBox);
+var Die = function () {
+    this.value = null;
+    this.div = null;
+    this.insert = function () {
+        this.div = document.createElement("div");
+        this.div.className = "dieDesign";
+        this.roll();
+        document.getElementById('mainDiv').appendChild(this.div);
+    }
+    this.roll = function () {
+        var num = Math.floor(Math.random() * 6) + 1;
+        this.value = num;
+        this.div.innerHTML = num;
+    }
+}
+var dice = []; //has to be in the global scope
+
+function addDie() {
+    var dieDesign = new Die();
+    var someDiv = document.createElement("div");
+    someDiv.className = "dieDesign";
+    dieDesign.div = someDiv;
+    dieDesign.roll();
+    document.getElementById('mainDiv').appendChild(someDiv);
+    dice.push(dieDesign);
 }
 
-var rollDice = function() {
-        var diceNumber = document.getElementsByClassName("dieDesign");
-        var ranNumber = String(Math.floor((Math.random() * 6) + 1));
-        diceNumber.appendChild(ranNumber); //invalid. Can't appendChild in this case
+function rollDice() {
+    for (var i = 0; i < dice.length; i++) {
+        dice[i].roll();
+    }
 }
