da3727c4e845e0c2ca0404404471849904ef8f6d
Jesla <jesslar415@gmail.com>
2016-03-06 21:50:40 -0600

    Starting Over

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-function Die (number) {
-    this.number = number;   
+var addDie = function() {
+        var dieDesign = document.getElementById("mainDiv");
+        var divBox = document.createElement("div");
+        divBox.className = "dieDesign";
+        dieDesign.appendChild(divBox);
 }
 
-var ranNumber = String(Math.floor((Math.random() * 6) + 1));
-var diceNumber = new Die (ranNumber);
+var rollDice = function() {
+        var diceNumber = document.getElementsByClassName("dieDesign");
+        var ranNumber = String(Math.floor((Math.random() * 6) + 1));
+        diceNumber.appendChild(ranNumber); //invalid. Can't appendChild in this case
+}
